4d6e36208092a1e93411ee6be45bb70c66713d31
MaheshH91 <holkarmahesh11@gmail.com>
2026-07-22 20:59:17 +0530

    Updated conftest

--- pageObjects/HomePage.py
+++ pageObjects/HomePage.py
@@ -1,15 +1,13 @@
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 class HomePage:
     def __init__(self, driver):
         self.driver = driver
         self.lnk_register_linktext = "Register"
         self.lnk_login_linktext = "Login"
         self.lnk_logout_linktext = "Logout"
-        self.my_account_xpath = "//span[text()='My Account']"
-
-    def clickMyAccount(self):
-        self.driver.find_element(By.XPATH, self.my_account_xpath).click()
+        self.my_account_xpath = "//a[@title='My Account']"
 
     def clickRegisterLink(self):
         self.driver.find_element(By.LINK_TEXT, self.lnk_register_linktext).click()
@@ -19,4 +17,8 @@ class HomePage:
 
     def clickLogoutLink(self):
         self.driver.find_element(By.LINK_TEXT, self.lnk_logout_linktext).click()
-    
+
+    def clickMyAccount(self):
+        WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, "//a[@title='My Account']"))
+        ).click()
